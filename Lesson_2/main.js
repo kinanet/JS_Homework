fdd7a4ee7507a4b32aab8d235d97d2d4754688b1
Aldungarov Yuriy <kinanet@bigmir.net>
2021-07-25 19:54:23 +0300

    Fixed second home work.

--- Lesson_2/main.js
+++ Lesson_2/main.js
@@ -4,43 +4,42 @@ var initialVariable = +prompt('Please insert initial value.', 0),
     deductible = +prompt('How much to subtract from the previous result?', 0),
     multiplier = +prompt('How much to multiply from the previous result?', 0),
     divisor = +prompt('How much to divide from the previous result?', 0);
-
- alert(((initialVariable + summand) - deductible) * multiplier / divisor);
+alert('Формула: ' + initialVariable + ' + ' + summand + ' - ' + deductible + ' * ' + multiplier + ' / ' + divisor + '.\n' +
+      'Результат: ' + (((initialVariable + summand) - deductible) * multiplier / divisor));
 
 // Second exercise
 var login,
     pass;
 
 login = prompt('Please enter the login.');
-if (!login) {
+
+if (login === 'Админ') {
+    pass = prompt('Please enter Your password.');
+
+    if (pass == 'Чёрный Властелин') {
+        alert('Welcome!');
+    }
+    else if (!pass) {
+        alert('Enter declined.');
+    }
+    else {
+        alert('Incorrect password.');
+    }
+}
+else if (!login) {
     alert('Enter declined.');
 }
-else {
-    if (login != 'Админ') {
-        alert('I don\'t know You.');
-    }
-    else{
-        pass = prompt('Please enter Your password.');
-
-        if (pass == 'Чёрный Властелин') {
-            alert('Welcome!');
-        }
-        else if (!pass) {
-            alert('Enter declined.');
-        }
-        else {
-            alert('Incorrect password.');
-        }
-    }
+else{
+    alert('I don\'t know You.');
 }
 
 // Third exercise
 var message;
 
 message = (login === 'Вася') ?
-    message = 'Привет' :
+    'Привет' :
     (login === 'Директор') ?
-        message = 'Здравствуйте' :
+        'Здравствуйте' :
         (login === '') ?
             'Нет логина' :
             '';
@@ -101,15 +100,18 @@ while (true) {
 while (true) {
     age = prompt('Please enter the age.');
 
-    if (age === ''){
+    if (!age) {
+        age = '-';
+        break;
+    }
+    else if (age === ''){
         alert('Age could not be empty.');
     }
     else if (+age !== +age){
         alert('Age should be valid number.');
     }
-    else if (!age) {
-        age = 0;
-        break;
+    else if ((+age < 6) || (+age > 100)) {
+        alert('Age should be between 6 and 100.');
     }
     else{
         age = +age;
@@ -123,7 +125,7 @@ isRetiree = ((isMan && (age >= 62)) || (!isMan && (age >= 57))) ? true : false;
 
 alert('Ваше ФИО: ' + lastName +' '+ firstName +' '+ patronymic +'\n'+
       'Ваш возраст в годах: ' + age +'\n'+
-      'Ваш возраст в днях: ' + (age * 365) +'\n'+
-      'Через 5 лет вам будет: ' + (age + 5) +'\n'+
+      'Ваш возраст в днях: ' + ((+age === +age) ? (age * 365) : '-') +'\n'+
+      'Через 5 лет вам будет: ' + ((+age === +age) ? (age + 5) : '-') +'\n'+
       'Ваш пол: ' + (isMan ? 'man' : 'woman') +'\n'+
-      'Вы на пенсии: ' + (isRetiree ? 'Yes' : 'No'));
+      'Вы на пенсии: ' + ((+age === +age) ? (isRetiree ? 'Yes' : 'No') : 'unknown'));
